ed59bc0ee6412b44b6404ca8ef2e71f2f61d76af
arpit-agr <arpit.agrawal2502@gmail.com>
2023-08-22 09:42:08 +0530

    configure markdown lib and remove unnecessary markdown plugins

--- config/plugins/markdown.js
+++ config/plugins/markdown.js
@@ -1,50 +1,28 @@
 const markdownIt = require('markdown-it');
-const markdownItPrism = require('markdown-it-prism');
 const markdownItAnchor = require('markdown-it-anchor');
-const markdownItClass = require('@toycode/markdown-it-class');
-const markdownItLinkAttributes = require('markdown-it-link-attributes');
-const markdownItEmoji = require('markdown-it-emoji');
 const markdownItFootnote = require('markdown-it-footnote');
-const markdownitMark = require('markdown-it-mark');
-const markdownitAbbr = require('markdown-it-abbr');
 const {slugifyString} = require('../utils');
 
 const markdownLib = markdownIt({
   html: true,
   breaks: true,
   linkify: true,
-  typographer: true
+  typographer: true // see https://github.com/markdown-it/markdown-it
 })
   .disable('code')
-  .use(markdownItPrism, {
-    defaultLanguage: 'plaintext'
-  })
   .use(markdownItAnchor, {
     slugify: slugifyString,
     tabIndex: false,
-    permalink: markdownItAnchor.permalink.headerLink({
-      class: 'heading-anchor'
+    level: [2, 3],
+    permalink: markdownItAnchor.permalink.linkAfterHeader({
+      style: 'visually-hidden',
+      class: 'anchor-link',
+      symbol: `<svg xmlns="http://www.w3.org/2000/svg" width="0.75em" height="0.75em" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="lucide lucide-hash" viewBox="0 0 24 24"><path d="M4 9h16M4 15h16M10 3 8 21M16 3l-2 18"/></svg>`,
+      assistiveText: title => `Section titled “${title}”`,
+      visuallyHiddenClass: 'visually-hidden',
+      wrapper: ['<div class="heading-wrapper">', '</div>']
     })
   })
-  .use(markdownItClass, {
-    ol: 'list',
-    ul: 'list'
-  })
-  .use(markdownItLinkAttributes, [
-    {
-      // match external links
-      matcher(href) {
-        return href.match(/^https?:\/\//);
-      },
-      attrs: {
-        target: '_blank',
-        rel: 'noopener'
-      }
-    }
-  ])
-  .use(markdownItEmoji)
-  .use(markdownItFootnote)
-  .use(markdownitMark)
-  .use(markdownitAbbr);
+  .use(markdownItFootnote);
 
 module.exports = markdownLib;
