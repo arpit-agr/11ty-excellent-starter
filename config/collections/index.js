f17d8ff7626a0fe7e69f6c2971393d6166232421
arpit-agr <arpit.agrawal2502@gmail.com>
2023-08-22 09:40:43 +0530

    add a collection for pages

--- config/collections/index.js
+++ config/collections/index.js
@@ -1,9 +1,15 @@
+/** Returns all pages as a collection. */
+const getAllPages = collection => {
+  return collection.getFilteredByGlob('./src/pages/*.md');
+};
+
 /** Returns all blog posts as a collection. */
 const getAllPosts = collection => {
-  const projects = collection.getFilteredByGlob('./src/posts/*.md');
-  return projects.reverse();
+  const posts = collection.getFilteredByGlob('./src/posts/*.md');
+  return posts.reverse();
 };
 
 module.exports = {
+  getAllPages,
   getAllPosts
 };
