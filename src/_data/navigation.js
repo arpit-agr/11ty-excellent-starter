7c7d3c750b9716b9cc692e8455508f8d2804327e
arpit-agr <arpit.agrawal2502@gmail.com>
2023-08-22 11:16:46 +0530

    update navigation.js

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,20 +1,12 @@
 module.exports = {
   top: [
     {
-      text: 'About',
-      url: '/about/'
+      text: 'About Us',
+      url: '#'
     },
     {
-      text: 'Get started',
-      url: '/get-started/'
-    },
-    {
-      text: 'Built with',
-      url: '/built-with/'
-    },
-    {
-      text: 'Blog',
-      url: '/blog/'
+      text: 'Contact Us',
+      url: '#'
     }
   ],
   bottom: [
