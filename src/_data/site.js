f764b9fa6f67a87e5c1b0d1ca6edc3bf90a6cf1e
arpit-agr <arpit.agrawal2502@gmail.com>
2023-08-22 11:14:27 +0530

    rename and update meta.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,24 +1,21 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Eleventy Excellent',
+  siteName: 'Excellent Starter',
   siteDescription:
     'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
   siteType: 'Person', // schema
   locale: 'en_EN',
   lang: 'en',
-  skipContent: 'Skip to content',
-  author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
-  authorEmail: '', // i.e. hola@lenesaile.com - email of the author
-  authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
-  themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
-  themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
+  author: '', // i.e. author's name. Must be set.
+  authorEmail: '', // i.e. email of the author
+  authorWebsite: '', // i.e. the personal site of the author
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
     opengraph_default_alt:
       'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ', // alt text for default meta image
     twitterSite: '', // i.e. @site - twitter profile of the site
-    twitterCreator: '', // i.e. @author -  twitter profile of the site
-    mastodonProfile: '' // i.e. https://front-end.social/@lene - url to your mastodon instance/profile
+    twitterCreator: '', // i.e. @author -  twitter profile of the author
+    mastodonProfile: '' // i.e. url to the author's mastodon instance/profile
   },
   blog: {
     // this is for the rss feed
@@ -31,7 +28,7 @@ module.exports = {
   },
   address: {
     // edit all presets or leave empty. They are being used in the pages for privacy.md and imprint.md
-    firma: 'Organization name',
+    firm: 'Organization name',
     street: '123 Main St.',
     city: 'Ciudad',
     state: 'Estado',
